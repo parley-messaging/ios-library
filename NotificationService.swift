0fd0e4af06315980467ab6e5eb71d0ca34b99121
Martijn van der Wal <m.vanderwal@webuildapps.com>
2021-10-12 10:58:23 +0200

    Re-added services

---
+++ NotificationService.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class NotificationService {
+    
+    func notificationsEnabled(completion: @escaping  ((Bool) -> ())) {
+        let current = UNUserNotificationCenter.current()
+
+        current.getNotificationSettings(completionHandler: { settings in
+            
+            switch settings.authorizationStatus {
+            case .authorized, .ephemeral, .provisional:
+                completion(true)
+            default:
+                completion(false)
+            }
+        })
+    }
+}
---
+++ PollingService.swift
@@ -0,0 +1,109 @@
+import Foundation
+
+protocol PollingServiceProtocol {
+    func startRefreshing()
+    func stopRefreshing()
+    var delegate: ParleyDelegate? { get set }
+}
+
+class PollingService: PollingServiceProtocol {
+    
+    private enum TimerInterval: TimeInterval {
+        case twoSeconds = 2
+        case fiveSeconds = 5
+        case tenSeconds = 10
+        case thirtySeconds = 30
+    }
+    
+    private var timer: Timer?
+    weak var delegate: ParleyDelegate?
+    private let messageRepository = MessageRepository()
+    private let messagesManager = Parley.shared.messagesManager
+    
+    private var loopRepeated: Int = 0 {
+        didSet {
+            if loopRepeated == 5 {
+                switch timerInterval {
+                case .twoSeconds:
+                    timerInterval = .fiveSeconds
+                case .fiveSeconds:
+                    timerInterval = .tenSeconds
+                case .tenSeconds:
+                    timerInterval = .thirtySeconds
+                default: break
+                }
+                setTimer(interval: timerInterval)
+                loopRepeated = 0
+            }
+        }
+    }
+    private var timerInterval: TimerInterval = .twoSeconds {
+        didSet { timer?.invalidate() }
+    }
+    
+    func startRefreshing() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            self.setTimer(interval: .twoSeconds)
+            self.addObservers()
+        }
+    }
+    
+    func stopRefreshing() {
+        timer?.invalidate()
+        loopRepeated = 0
+        timerInterval = .twoSeconds
+        timer = nil
+        removeObservers()
+    }
+    
+    private func setTimer(interval: TimerInterval) {
+        timer = Timer.scheduledTimer(withTimeInterval: interval.rawValue, repeats: true) { [weak self] _ in
+            self?.refreshFeed()
+        }
+    }
+    
+    private func refreshFeed() {
+        guard let id = messagesManager.lastMessage?.id, timer?.isValid == true else { return }
+        messageRepository.findAfter(id, onSuccess: { [weak self, weak delegate, weak messagesManager] messageCollection in
+            guard !messageCollection.messages.isEmpty else {
+                self?.loopRepeated += 1
+                return
+            }
+            self?.loopRepeated = 0
+            self?.timerInterval = .twoSeconds
+            messagesManager?.handle(messageCollection, .all)
+            delegate?.didReceiveMessages()
+        }, onFailure: { error in
+            print("Polling failed")
+        })
+    }
+}
+
+internal extension PollingService {
+    
+    // MARK: Observers
+    private func addObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(willEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didEnterBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+    }
+   
+    private func removeObservers() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc private func willEnterForeground() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            self.timer?.invalidate()
+            self.timer = nil
+            self.setTimer(interval: self.timerInterval)
+        }
+    }
+    
+    @objc private func didEnterBackground() {
+        DispatchQueue.main.async { [weak self] in
+            self?.timer?.invalidate()
+        }
+    }
+}
