2560411ae3b28c1e7a75dcc59db068fc78ef14ee
Martijn van der Wal <m.vanderwal@webuildapps.com>
2021-10-21 15:48:59 +0200

    code cleanup

--- Source/Data/Models/MessageButton.swift
+++ Source/Data/Models/MessageButton.swift
@@ -1,6 +1,6 @@
 import ObjectMapper
 
-public class MessageButton: Mappable, Equatable {
+public class MessageButton: Mappable {
    
     var title: String!
     var payload: String!
@@ -18,13 +18,5 @@ public class MessageButton: Mappable, Equatable {
         self.title      <- map["title"]
         self.payload    <- map["payload"]
         self.type       <- map["type"]
-    }
-    
-    public static func == (lhs: MessageButton, rhs: MessageButton) -> Bool {
-        if lhs.title == rhs.title, lhs.payload == rhs.payload, lhs.type == rhs.type {
-            return true
-        } else {
-            return false
-        }
     }
 }
