e97b9d2bf2cd58098e19f77dca13893ad5d664b4
Martijn van der Wal <m.vanderwal@webuildapps.com>
2021-10-22 14:56:15 +0200

    Re-added files

---
+++ Source/Data/Models/ParleyImageType.swift
@@ -0,0 +1,37 @@
+import Foundation
+import UIKit
+
+internal enum ParleyImageType: String, CaseIterable {
+    case png
+    case gif
+    case jpg
+    
+    /// Returns a mime type
+    /// for example: "image/png"
+    var mimeType: String {
+        switch self {
+        case .png:
+            return "image/png"
+        case .gif:
+            return "image/gif"
+        case .jpg:
+            return "image/gif"
+        }
+    }
+    
+    /// Appends a dot (.) before the file extension.
+    /// example: .png
+    var fileExtension: String {
+        return ".\(self.rawValue)"
+    }
+    
+    
+    /// Returns a ParleyImageType from a given URL
+    /// Defaults to .jpg.
+    /// - Parameter url: Local URL of the image
+    /// - Returns: ParleyImageType
+    static func map(from url: URL) -> ParleyImageType {
+        let imageName = url.lastPathComponent
+        return ParleyImageType.allCases.first { imageName.contains($0.fileExtension) } ?? .jpg
+    }
+}
