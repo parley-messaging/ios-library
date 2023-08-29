2c7ab1c9d0dca2218332bc97d2ec1d9d7c0ead3f
martijnvdwal <martijnvdwal2001@gmail.com>
2023-08-29 09:41:35 +0200

    Changes in file references

--- Source/Extensions/StringExtension.swift
+++ Source/Extensions/StringExtension.swift
@@ -10,4 +10,23 @@ extension String {
 
         return ceil(boundingBox.height)
     }
+    
+    /// Appends a string to the current string while maintaining a maximum of a single space character between words.
+    /// - Parameter string: the string to be appended at the end of `self`.
+    ///
+    /// - Important: The input string **shouldn't** start with a space.
+    mutating func appendWithCorrectSpacing(_ string: String) {
+        guard !string.isEmpty else { return }
+        guard !isEmpty else { self = string ; return }
+        
+        if let lastCharacter = self.last, lastCharacter != " " {
+            self.append(" ")
+        }
+        
+        self.append(string)
+    }
+    
+    internal var localized: String {
+        NSLocalizedString(self, bundle: Bundle.current, comment: "")
+    }
 }
