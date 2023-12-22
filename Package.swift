3c6146353223d6e554de00cf13e23bd021b6832f
mat1th <6486389+mat1th@users.noreply.github.com>
2023-12-22 11:19:07 +0100

    feat: remove ObjectMapper from package.swift and Podfile;

--- Package.swift
+++ Package.swift
@@ -17,7 +17,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1")),
         .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.3.0")),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.2.0")),
         .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift.git", .upToNextMajor(from: "5.1.0")),
         .package(url: "https://github.com/bmoliveira/MarkdownKit.git", .upToNextMajor(from: "1.7.0"))
     ],
@@ -27,7 +26,6 @@ let package = Package(
             dependencies: [
                 "Alamofire",
                 "AlamofireImage",
-                "ObjectMapper",
                 "Reachability",
                 "MarkdownKit"
             ],
