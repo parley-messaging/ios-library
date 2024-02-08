5d7f2c96e97127aa0395d94bddbdced71a1acb08
Martijn van der Wal <martijnvdwal2001@gmail.com>
2024-02-08 13:11:51 +0100

    Removed redundant code

--- Sources/Parley/Extensions/BundleExtension.swift
+++ Sources/Parley/Extensions/BundleExtension.swift
@@ -1,14 +1,6 @@
 import Foundation
 
-private class BundleFinder {}
-
 extension Bundle {
     
-    static var current: Bundle = {
-        #if SWIFT_PACKAGE
-            return Bundle.module
-        #endif
-        
-        return Bundle(for: BundleFinder.self)
-    }()
+    static let current: Bundle = .module
 }
