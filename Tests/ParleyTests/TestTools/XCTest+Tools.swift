bd3025d4a2186ee96229a7b9a6be2a63a1836dab
Ton Heijligers <ton.heijligers@triodos.com>
2024-05-28 13:05:08 +0200

    Combine two XCTestCase extensions

--- Tests/ParleyTests/TestTools/XCTest+Tools.swift
+++ Tests/ParleyTests/TestTools/XCTest+Tools.swift
@@ -10,4 +10,19 @@ extension XCTestCase {
         exp.isInverted = true
         wait(for: [exp], timeout: timeout)
     }
+    
+    public func wait(
+        _ timeout: TimeInterval = 2.0,
+        for expression: @escaping () -> Bool,
+        fileID: String = #fileID,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let predicate = NSPredicate { _, _ -> Bool in
+            expression()
+        }
+        let expectation = expectation(for: predicate, evaluatedWith: nil)
+        expectation.expectationDescription = "\(fileID):\(line)"
+        wait(for: [expectation], timeout: timeout)
+    }
 }
