72c05b250ec141059f2b21e3a12c5256e4110572
Martijn van der Wal <martijnvdwal2001@gmail.com>
2025-01-31 11:02:07 +0100

    Added async alternatives to MessageRemoteService

--- Sources/Parley/Data/Remotes/MessageRemoteService.swift
+++ Sources/Parley/Data/Remotes/MessageRemoteService.swift
@@ -92,3 +92,42 @@ final class MessageRemoteService {
         remote.execute(.get, path: "media/\(id)", type: type, result: result)
     }
 }
+
+// MARK: Async Methods
+extension MessageRemoteService {
+    
+    func find(_ id: Int) async throws -> Message {
+        try await remote.execute(.get, path: "messages/\(id)")
+    }
+    
+    func findAll() async throws -> MessageCollection {
+        try await remote.execute(.get, path: "messages", keyPath: nil)
+    }
+    
+    func findBefore(_ id: Int) async throws -> MessageCollection {
+        try await remote.execute(.get, path: "messages/before:\(id)", keyPath: nil)
+    }
+    
+    func findAfter(_ id: Int) async throws -> MessageCollection {
+        try await remote.execute(.get, path: "messages/after:\(id)", keyPath: nil)
+    }
+    
+    func store(_ message: Message) async throws -> Message {
+        try await remote.execute(.post, path: "messages", body: message)
+    }
+    
+    func upload(data: Data, type: ParleyMediaType, fileName: String) async -> Result<MediaResponse, Error> {
+        do {
+            let mediaResponse: MediaResponse = try await remote.execute(
+                path: "media",
+                data: data,
+                name: "media",
+                fileName: fileName,
+                type: type
+            )
+            return .success(mediaResponse)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
