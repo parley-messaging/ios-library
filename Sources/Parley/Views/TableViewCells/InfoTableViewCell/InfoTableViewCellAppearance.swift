fd3c270365254a8cc6b6c34ac5ed878dbaa7f70d
Martijn van der Wal <martijnvdwal2001@gmail.com>
2025-07-18 12:18:13 +0200

    Renamed default to legacy, and made adaptive the new default

--- Sources/Parley/Views/TableViewCells/InfoTableViewCell/InfoTableViewCellAppearance.swift
+++ Sources/Parley/Views/TableViewCells/InfoTableViewCell/InfoTableViewCellAppearance.swift
@@ -6,7 +6,7 @@ public struct InfoTableViewCellAppearance: Sendable {
     public var position: Position
     
     public enum Position: Sendable {
-        case `default`
+        case legacy
         case adaptive
     }
     
@@ -14,7 +14,7 @@ public struct InfoTableViewCellAppearance: Sendable {
     init(
         textViewAppearance: ParleyTextViewAppearance = ParleyTextViewAppearance(textColor: UIColor(red: 0.29, green: 0.37, blue: 0.51, alpha: 1.0)),
         contentInset: UIEdgeInsets? = UIEdgeInsets(top: 16, left: 32, bottom: 16, right: 32),
-        position: Position = .default
+        position: Position = .adaptive
     ) {
         self.textViewAppearance = textViewAppearance
         self.contentInset = contentInset
